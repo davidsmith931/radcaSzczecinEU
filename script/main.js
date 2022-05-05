ee06c631e4c473a12a26357e95423458d2df47b8
davidsmith931 <radoslaw.mikus@gmail.com>
2022-05-05 23:59:00 +0200

    Added smooth scroll

--- script/main.js
+++ script/main.js
@@ -5,3 +5,13 @@ button.addEventListener("click", () =>
     "Formularz jest aktualnie wyłączony, prosimy o kontakt pod adresem mailowym kontakt@radcaszczecin.eu"
   )
 );
+
+// POLYFILL SCROLL
+// ----------------------------------
+
+document.querySelector("html").style.scrollBehavior = "auto";
+const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+  speed: 1200,
+  speedAsDuration: true,
+  // offset: 50,
+});
