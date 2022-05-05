46dbfead7fdcfa54d8f56eb60dff069255473240
davidsmith931 <radoslaw.mikus@gmail.com>
2022-05-05 23:47:06 +0200

    Added contactForm

---
+++ script/main.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("#send");
+
+button.addEventListener("click", () =>
+  alert(
+    "Formularz jest aktualnie wyłączony, prosimy o kontakt pod adresem mailowym kontakt@radcaszczecin.eu"
+  )
+);
