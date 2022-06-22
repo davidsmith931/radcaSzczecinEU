c5d24ece604a764fc5674f59e3c897ad40eb0a01
davidsmith931 <radoslaw.mikus@gmail.com>
2022-06-22 23:08:46 +0200

    Fixed responsivity, finished SCSS

--- script/main.js
+++ script/main.js
@@ -4,28 +4,8 @@ document
 
 // POLYFILL SCROLL
 // ----------------------------------
-
 document.querySelector("html").style.scrollBehavior = "auto";
 const scroll = new SmoothScroll('.navbar a[href*="#"]', {
   speed: 1200,
   speedAsDuration: true,
-  // offset: 50,
 });
-
-// // Prevents window from moving on touch on older browsers.
-// window.addEventListener(
-//   "touchmove",
-//   function (event) {
-//     event.preventDefault();
-//   },
-//   false
-// );
-
-// // Allows content to move on touch.
-// document.querySelector(".body-container").addEventListener(
-//   "touchmove",
-//   function (event) {
-//     event.stopPropagation();
-//   },
-//   false
-// );
